14e4635ff8758d1c36982bec11414bd73208eff2
Mike Eberhart <42505829+MikeEberhart@users.noreply.github.com>
2024-09-16 04:01:07 -0400

    Added bool; Tweaked save functions;
    Added bool to check for selected item; Edited SaveColorData function to stop from saving already saved color;

--- app/src/main/java/com/example/homework02_program1/MainActivity.java
+++ app/src/main/java/com/example/homework02_program1/MainActivity.java
@@ -8,7 +8,6 @@ package com.example.homework02_program1;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -16,12 +15,9 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
-import java.io.ObjectOutput;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -36,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_j_green_progress;
     TextView tv_j_blue_label;
     TextView tv_j_blue_progress;
+    TextView tv_j_error_display;
     SeekBar sb_j_red_seekbar;
     SeekBar sb_j_green_seekbar;
     SeekBar sb_j_blue_seekbar;
@@ -46,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<ColorData> main_list_of_colors;
     ColorDisplayAdapter adapter_main;
     Boolean main_text_is_black;
+    Boolean main_color_selected;
     int rgb_red_value;
     int rgb_green_value;
     int rgb_blue_value;
@@ -62,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         tv_j_hex_display = findViewById(R.id.tv_v_hex_display);
         tv_j_red_label = findViewById(R.id.tv_v_red_label);
         tv_j_red_progress = findViewById(R.id.tv_v_red_progress);
+        tv_j_error_display = findViewById(R.id.tv_v_error_display);
         sb_j_red_seekbar = findViewById(R.id.sb_v_red_seekbar);
         tv_j_green_label = findViewById(R.id.tv_v_green_label);
         tv_j_green_progress = findViewById(R.id.tv_v_green_progress);
@@ -73,57 +72,50 @@ public class MainActivity extends AppCompatActivity {
         btn_j_save_color = findViewById(R.id.btn_v_save_color);
         btn_j_delete = findViewById(R.id.btn_v_delete);
         iv_j_hex_underline = findViewById(R.id.iv_v_hex_underline);
+        tv_j_error_display.setVisibility(View.INVISIBLE);
         rgb_red_value = 255;
         rgb_green_value = 255;
         rgb_blue_value = 255;
         pos_of_selected = 0;
         main_text_is_black = true;
+        main_color_selected = false;
         main_list_of_colors = new ArrayList<ColorData>();
         PopulateListView();
         SeekBarEvent();
-        ButtonTouchEvents();
+        ButtonClickEvents();
     }
 
     // function used to listen for the onTouch events
-    private void ButtonTouchEvents() {
-        btn_j_random.setOnTouchListener(new View.OnTouchListener() {
+    private void ButtonClickEvents() {
+        btn_j_random.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    Random ran = new Random();
-                    int random_red = ran.nextInt(256);
-                    int random_green = ran.nextInt(256);
-                    int random_blue = ran.nextInt(256);
-                    SettingBackgroundAndHex(random_red, random_green, random_blue);
-                    v.performClick();
-                    return true;
-                }
-                return true;
+            public void onClick(View v) {
+                Random ran = new Random();
+                int random_red = ran.nextInt(256);
+                int random_green = ran.nextInt(256);
+                int random_blue = ran.nextInt(256);
+                SettingBackgroundAndHex(random_red, random_green, random_blue);
+                main_color_selected = false;
             }
         });
-        btn_j_save_color.setOnTouchListener(new View.OnTouchListener() {
+        btn_j_save_color.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    SaveColorData();
-                    ResetToDefault();
-                    v.performClick();
-                    return true;
-                }
-                return true;
+            public void onClick(View v) {
+                SaveColorData();
+                SettingBackgroundAndHex(255,255,255);
+                main_color_selected = false;
             }
         });
-        btn_j_delete.setOnTouchListener(new View.OnTouchListener() {
+        btn_j_delete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
+            public void onClick(View v) {
+                if(main_color_selected)
+                {
                     main_list_of_colors.remove(pos_of_selected);
                     adapter_main.notifyDataSetChanged();
-                    ResetToDefault();
-                    v.performClick();
-                    return true;
+                    SettingBackgroundAndHex(255,255,255);
+                    main_color_selected = false;
                 }
-                return true;
             }
         });
         lv_j_display_colors.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -131,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ColorData cd_list_pos = main_list_of_colors.get(position);
                 pos_of_selected = position;
+                main_color_selected = true;
                 SettingBackgroundAndHex(cd_list_pos.getRed(), cd_list_pos.getGreen(), cd_list_pos.getBlue());
                 Log.d("--ITEM-CLICK--", cd_list_pos.getHex());
             }
@@ -144,16 +137,14 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 tv_j_red_progress.setText(Integer.toString(progress));
                 rgb_red_value = progress;
-                ColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
+                GuiColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
                 SettingBackgroundAndHex(rgb_red_value, rgb_green_value, rgb_blue_value);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
         sb_j_green_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -161,16 +152,14 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 tv_j_green_progress.setText(Integer.toString(progress));
                 rgb_green_value = progress;
-                ColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
+                GuiColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
                 SettingBackgroundAndHex(rgb_red_value, rgb_green_value, rgb_blue_value);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
         sb_j_blue_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -178,16 +167,14 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 tv_j_blue_progress.setText(Integer.toString(progress));
                 rgb_blue_value = progress;
-                ColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
+                GuiColorSwitch(rgb_red_value, rgb_green_value, rgb_blue_value);
                 SettingBackgroundAndHex(rgb_red_value, rgb_green_value, rgb_blue_value);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
     }
@@ -203,8 +190,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // function used to switch the GUI colors back and forth from black and white depending rgb values
-    private void ColorSwitch(int r, int g, int b) {
-        if(r < 100 && g < 100 && b < 100) {
+    // I made the threshold for switch the colors back and forth at 120 becuase even at 100
+    // solid colors it seemed hard to see the black GUI, mainly the solid blue.
+    private void GuiColorSwitch(int r, int g, int b) {
+        if(r < 120 && g < 120 && b < 120) {
             tv_j_hex_label.setTextColor(Color.WHITE);
             tv_j_hex_display.setTextColor(Color.WHITE);
             tv_j_hex_display.setBackgroundResource(R.drawable.wht_sqr_transparent_label);
@@ -249,37 +238,47 @@ public class MainActivity extends AppCompatActivity {
         adapter_main = new ColorDisplayAdapter(this, main_list_of_colors);
         lv_j_display_colors.setAdapter(adapter_main);
     }
-
-    // function used to reset everything back to default settings when color is saved
-    private void ResetToDefault() {
-        int r = 255;
-        int g = 255;
-        int b = 255;
-        cLayout.setBackgroundColor(Color.rgb(r, g, b));
-        String hex = String.format("#%02X%02X%02X",r,g,b);
-        sb_j_red_seekbar.setProgress(r);
-        sb_j_green_seekbar.setProgress(g);
-        sb_j_blue_seekbar.setProgress(b);
-        tv_j_hex_display.setText(hex);
+    private Boolean ContainsColorAlready(int red, int green, int blue) {
+        Log.d("---ContainsColor---", "ContainsColor");
+        String hex = String.format("#%02X%02X%02X", red, green, blue);
+        for (ColorData cd_hex : main_list_of_colors) {
+            if (cd_hex.getHex().equals(hex)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void SaveColorData() {
         int r = rgb_red_value;
         int g = rgb_green_value;
         int b = rgb_blue_value;
-        String hex = String.format("#%02X%02X%02X",r,g,b);
-//        Object obj =; // trying to make sure new data doesn't already exist in the ArrayList
-//        if()
-//        {
-//
-//        }
-        ColorData new_color = new ColorData();
-        new_color.setHex(hex);
-        new_color.setRed(r);
-        new_color.setGreen(g);
-        new_color.setBlue(b);
-        new_color.setTextColor(main_text_is_black);
-        main_list_of_colors.add(new_color);
-        adapter_main.notifyDataSetChanged();
+        if(!ContainsColorAlready(r,g,b)) {
+            String hex = String.format("#%02X%02X%02X",r,g,b);
+            ColorData new_color = new ColorData();
+            new_color.setHex(hex);
+            new_color.setRed(r);
+            new_color.setGreen(g);
+            new_color.setBlue(b);
+            new_color.setTextColor(main_text_is_black);
+            main_list_of_colors.add(new_color);
+            adapter_main.notifyDataSetChanged();
+            Log.d("---SaveColorData---","SaveColorData");
+        }
+        else{
+//            tv_j_error_display.setVisibility(View.VISIBLE);
+            Log.d("---DontSaveColorData---","DontSaveColorData");
+//            for(int i = 0; i < 5; i++){
+//                tv_j_error_display.setVisibility(View.INVISIBLE);
+//                try {
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                tv_j_error_display.setVisibility(View.VISIBLE);
+//            }
+//            tv_j_error_display.setVisibility(View.INVISIBLE);
+
+        }
     }
 }
